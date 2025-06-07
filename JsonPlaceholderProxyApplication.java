67c0cfc39200cdf6f65b4b53173480e1bd43b022
temptation4 <30442527+temptation4@users.noreply.github.com>
2025-06-07 15:57:48 +0530

    Add files via upload

---
+++ JsonPlaceholderProxyApplication.java
@@ -0,0 +1,11 @@
+package com.hgs.jsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceholderProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JsonPlaceholderProxyApplication.class, args);
+    }
+}
